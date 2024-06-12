33ae85d738366eed8992fce986eb7aa0723d290e
ADSD1804 <133187061+ADSD1804@users.noreply.github.com>
2024-06-11 22:47:13 -0500

    Update service.service.ts

--- Segundo-mini-proyecto/consumo_api/src/app/services/service.service.ts
+++ Segundo-mini-proyecto/consumo_api/src/app/services/service.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const URL = 'https://backend-nt96.onrender.com';
+const URL = 'https://api.escuelajs.co/api/v1/categories';
 
 @Injectable({
   providedIn: 'root'
